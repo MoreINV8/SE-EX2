88e48113d7094e2e7d58920341fa3b1122b4400d
INV8 <chaiyapat.sr@ku.th>
2024-07-10 18:56:07 +0700

    exercise 2

---
+++ src/GumballMachine.java
@@ -0,0 +1,83 @@
+public class GumballMachine {
+        private enum State {
+            NoQuarter,
+            HasQuarter,
+            GumballSolid,
+            OutOfGumball
+        }
+
+
+        private int gumballs;
+        private State state;
+
+
+        public GumballMachine(int gumballs) {
+            this.gumballs = gumballs;
+            this.state = State.NoQuarter;
+        }
+
+
+        public void insertQuarter() {
+            if (state.equals(State.OutOfGumball)) {
+                System.out.println("You cant insert a quarter, the machine is sold out");
+            } else if (state.equals(State.NoQuarter)) {
+                System.out.println("You inserted a quarter");
+                state = State.HasQuarter;
+            } else {
+                System.out.println("Already have a Quarter!!");
+            }
+        }
+
+
+        public void ejectQuarter() {
+            if (state.equals(State.HasQuarter)) {
+                state = State.NoQuarter;
+                System.out.println("Quarter returned");
+            } else {
+                System.out.println("You haven't inserted a quarter");
+            }
+        }
+
+
+        public void turn() {
+            if (state.equals(State.OutOfGumball)){
+                System.out.println("You turned, but there are no gumballs");
+            } else if (state.equals(State.HasQuarter)) {
+                System.out.println("You turned...");
+                gumballs -= 1;
+                state = State.GumballSolid;
+                dispenseGumball();
+            } else {
+                System.out.println("You turned but there's no quarter");
+            }
+        }
+
+
+        public void dispenseGumball() {
+            if (state.equals(State.GumballSolid)) {
+                System.out.println("A gumball comes rolling out the slot");
+                if (gumballs > 0) {
+                    state = State.NoQuarter;
+                } else {
+                    System.out.println("Oops, out of gumballs");
+                    state = State.OutOfGumball;
+                }
+            }
+        }
+
+        public void report() {
+            System.out.println("Mighty Gumball, Inc.");
+            System.out.println("Java-enabled Standing Gumball Model #2004");
+            System.out.println("Inventory: " + gumballs + " gumballs");
+            if (state.equals(State.NoQuarter)) {
+                System.out.println("Machine is waiting for quarter");
+            } else {
+                System.out.println("Machine is sold out");
+            }
+            System.out.println();
+        }
+}
+
+/**
+ * @author 6510450305 chaiyapat
+ */
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        GumballMachine mightyGumball = new GumballMachine(5);
+        mightyGumball.report();
+        mightyGumball.insertQuarter();
+        mightyGumball.turn();
+        System.out.println();
+
+        mightyGumball.report();
+        mightyGumball.insertQuarter();
+        mightyGumball.ejectQuarter();
+        mightyGumball.turn();
+        System.out.println();
+
+        mightyGumball.report();
+        mightyGumball.insertQuarter();
+        mightyGumball.turn();
+        mightyGumball.insertQuarter();
+        mightyGumball.turn();
+        mightyGumball.turn();
+        System.out.println();
+
+        mightyGumball.report();
+        mightyGumball.insertQuarter();
+        mightyGumball.turn();
+        mightyGumball.insertQuarter();
+        mightyGumball.turn();
+        mightyGumball.insertQuarter();
+        mightyGumball.turn();
+
+        System.out.println();
+
+        mightyGumball.report();
+    }
+}
+
+/**
+ * @author 6510450305 chaiyapat
+ */
